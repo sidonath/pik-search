783de9ad4185e90415d7975130c510ffad569f13
Damir Zekic <damirz@gmail.com>
2014-02-25 10:03:04 +0100

    Prepare URL for search

--- main.rb
+++ main.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
+require 'uri'
+
+def prepare_uri(search)
+  search_uri = URI.parse('http://www.pik.ba/?sort_order=ASC&sort_po=cijena')
+  search_uri.query += "&trazilica=#{search}"
+  search_uri
+end
 
 print "Unesite pojam za pretragu: "
 search = gets.chomp
+
+uri = prepare_uri(search)
+puts "URL za pretragu je #{uri.to_s}"
