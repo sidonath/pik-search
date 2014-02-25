d176ad1c232c3dc987fa155de8eff73d1352fb37
Damir Zekic <damirz@gmail.com>
2014-02-25 10:08:55 +0100

    Escape query string values

--- main.rb
+++ main.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 require 'uri'
+require 'cgi'
 
 def prepare_uri(search)
+  search = CGI.escape(search)
+
   search_uri = URI.parse('http://www.pik.ba/?sort_order=ASC&sort_po=cijena')
   search_uri.query += "&trazilica=#{search}"
   search_uri
